ef717cf0edc2911d036456f5bc171c45e412cdfd
Antoine Zimmermann <antoine.zimmermann@epitech.eu>
2023-12-18 11:38:09 +0100

    Day 01 Js

---
+++ Day 01/Part 1/main.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var readline = require('readline');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('input.txt'),
+    output: process.stdout,
+    terminal: false,
+});
+
+let total = 0;
+
+rl.on('line', (line) => {
+    let digit1, digit2;
+    let tmp = '';
+
+    for (const char of line) {
+        tmp += char;
+
+        if (!isNaN(char) && !digit1)
+            digit1 = parseInt(char);
+
+        if (!isNaN(char))
+            digit2 = parseInt(char);
+    }
+
+    total += digit1 * 10 + digit2;
+    console.log(total);
+});
---
+++ Day 01/Part 2/main.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+var readline = require('readline');
+
+const digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+function includeDigit(str) {
+    let result;
+
+    for (const digit of digits) {
+        if (str.includes(digit)) {
+            result = digits.indexOf(digit) + 1;
+        }
+    }
+
+    return result;
+}
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('input.txt'),
+    output: process.stdout,
+    terminal: false,
+});
+
+let total = 0;
+
+rl.on('line', (line) => {
+    let digit1, digit2;
+    let tmp = '';
+
+    for (const char of line) {
+        tmp += char;
+        const digit = includeDigit(tmp);
+
+        if (digit && !digit1)
+            digit1 = digit;
+        else if (!isNaN(char) && !digit1)
+            digit1 = parseInt(char);
+
+        if (digit) {
+            digit2 = digit;
+            tmp = tmp[tmp.length - 1];
+        }
+        if (!isNaN(char))
+            digit2 = parseInt(char);
+    }
+
+    total += digit1 * 10 + digit2;
+    console.log(total);
+});
